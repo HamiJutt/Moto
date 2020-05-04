b95edcb61d227e00da1e6e160ce5a62003c2f084
HamiJutt <64637766+HamiJutt@users.noreply.github.com>
2020-05-04 19:42:21 +0500

    Update and rename zeroboy.py to Moto1.py

--- Moto1.py
+++ Moto1.py
@@ -75,10 +75,10 @@ logo = """ -----------------------------•◈•
 ─██████████████─██████████─██████──██████─██████──██████─
 ─────────────────────────────────────────────────────────
 =======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mAliha
+\033[1;96mInstagram \033[1;93m: \033[1;FlowQueen
+\033[1;96mFacebook  \033[1;93m: \033[1; Aliha Ali
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Aliha/Moto
 \033[1;91m======================================="""
 
 def tik():
@@ -99,15 +99,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mAliha
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowQueen
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Aliha Ali
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Aliha/Moto
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Aliha"
+CorrectPassword = "Aliha"
 
 loop = 'true'
 while (loop == 'true'):
